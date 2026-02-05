04c3e37c965a3cdaac87bc74f1b7ec49aec4876e
Mafra <maframano@hotmail.com>
2026-02-05 12:25:48 -0300

    Adicionando relatorio HTML

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,11 @@ module.exports = defineConfig({
   e2e: {
     //video: true,
     baseUrl: "http://localhost:4000",
+    reporter: 'cypress-mochawesome-reporter',
     //baseUrl: process.env.URL || Cypress.env('http://localhost:4000')adicionado para variaveis de ambiente
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
   },
 });
--- cypress/support/commands/transferencia.js
+++ cypress/support/commands/transferencia.js
@@ -1,7 +1,7 @@
-Cypress.Command.add('RealizarTransferencia', (contaOrigem, contaDestino, valor) => [
-  cy.SelecionarOpçãoNaComboBox('conta-origem' , contaOrigem)
-  cy.SelecionarOpçãoNaComboBox('conta-destino' , contaDestino)
+Cypress.Commands.add('realizarTransferencia', (contaOrigem, contaDestino, valor) => {
+  cy.SelecionarOpçaoNaComboBox('conta-origem' , contaOrigem)
+  cy.SelecionarOpçaoNaComboBox('conta-destino' , contaDestino)
   cy.get('#valor').click().type(valor)
   cy.contains('button', 'Transferir').click()
 
-])
+})
